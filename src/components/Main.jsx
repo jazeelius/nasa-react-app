ee80c26b21d3bee38fd1620b278ceec895e3db2e
jazeelius <42842694+jazeelius@users.noreply.github.com>
2024-06-09 17:27:50 -0400

    bug fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,8 @@
-export default function Main(){
+export default function Main(props) {
     const { data } = props
     return (
-        
         <div className="imgContainer">
-            <img src={data.hdurl} alt="mars-demo-picture" className="bgImage"/>
+            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
         </div>
     )
 }
